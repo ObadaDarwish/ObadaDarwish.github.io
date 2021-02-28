ccdc3fdc72e689d0392cc24d0d1d91ba971dfec2
Obada Darwish <obada_567@hotmail.co.uk>
2021-03-01 01:22:08 +0200

    update

--- script.js
+++ script.js
@@ -4,12 +4,16 @@
 $(document).ready(function () {
     $("#name").fadeIn(1000);
     $("#jobTitle").fadeIn(2000);
+    let currentDate = new Date();
+    let startDate = new Date('07-01-2016');
+    $("#yearCounter").text(currentDate.getFullYear() - startDate.getFullYear());
 
     $('#nav_home').click(function () {
         $('html').animate({scrollTop: 0}, 1000);
     });
     $('#aboutMe').slideDown(1500);
-    $('#aboutMe').css('display','flex');
+    $('#aboutMe').css('display', 'flex');
+
     $('#nav_project').click(function () {
         $('html').animate({scrollTop: $('.projects_block').offset().top}, 1000);
     });
@@ -17,6 +21,169 @@ $(document).ready(function () {
     $('#scroll_up').click(function () {
         $('html').animate({scrollTop: 0}, 1000);
     });
+
+    var gaugeOptions = {
+        chart: {
+            type: 'solidgauge',
+            backgroundColor: '#eee',
+            height: 400
+        },
+
+        title: null,
+
+        pane: {
+            center: ['50%', '85%'],
+            size: '140%',
+            startAngle: -90,
+            endAngle: 90,
+            background: {
+                backgroundColor: '#a5a5a5',
+                innerRadius: '60%',
+                outerRadius: '100%',
+                shape: 'arc'
+            }
+        },
+
+        exporting: {
+            enabled: false
+        },
+
+        tooltip: {
+            enabled: true
+        },
+
+        // the value axis
+        yAxis: {
+            stops: [
+                [0.3, '#DF5353'], // red
+                [0.5, '#DDDF0D'], // yellow
+                [0.7, '#97ee83'], // yellow
+                [0.9, '#55BF3B'] // green
+            ],
+            lineWidth: 0,
+            tickWidth: 0,
+            minorTickInterval: null,
+            tickAmount: 2,
+            title: {
+                y: -70
+            },
+            labels: {
+                y: 16
+            }
+        },
+
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    y: 5,
+                    borderWidth: 0,
+                    useHTML: true,
+                }
+            }
+        }
+    };
+// The speed gauge
+    var chartSpeed = Highcharts.chart('container-speed', Highcharts.merge(gaugeOptions, {
+        yAxis: {
+            min: 0,
+            max: 100,
+            title: {
+                text: 'Proficiency',
+            }
+        },
+
+        credits: {
+            enabled: false
+        },
+
+        series: [{
+            name: 'Proficiency',
+            data: [90],
+            dataLabels: {
+                format:
+                    '<div style="text-align:center">' +
+                    '<span style="font-size:25px">{y}</span><br/>' +
+                    '<span style="font-size:12px;opacity:0.4">%</span>' +
+                    '</div>'
+            },
+            tooltip: {
+                valueSuffix: '%'
+            }
+        }]
+
+    }));
+    $('.skill_wrapper').click(function () {
+        let point = chartSpeed.series[0].points[0];
+        const nodes = document.querySelectorAll('.skill_wrapper');
+        nodes.forEach(node => {
+            node.classList.remove('active');
+        })
+        this.classList.add('active');
+        if (chartSpeed) {
+            switch (this.id) {
+                case 'angular': {
+                    point.update(90);
+                    break;
+                }
+                case 'react': {
+                    point.update(90)
+                    break
+                }
+                case 'redux': {
+                    point.update(90)
+                    break
+                }
+                case 'node': {
+                    point.update(80)
+                    break
+                }
+                case 'js': {
+                    point.update(98)
+                    break
+                }
+                case 'ts': {
+                    point.update(80)
+                    break
+                }
+                case 'html': {
+                    point.update(98)
+                    break
+                }
+                case 'css': {
+                    point.update(98)
+                    break
+                }
+                case 'scss': {
+                    point.update(75)
+                    break
+                }
+                case 'mongodb': {
+                    point.update(60)
+                    break
+                }
+                case 'mysql': {
+                    point.update(60)
+                    break
+                }
+                case 'git': {
+                    point.update(75)
+                    break
+                }
+                case 'gulp': {
+                    point.update(45)
+                    break
+                }
+                default: {
+                    point.update(90)
+                }
+
+            }
+
+        }
+
+
+    });
+
 
 });
 
@@ -36,20 +203,21 @@ function hoverProject(id) {
         }
     );
 }
+
 function openProfile(url) {
     window.open(url, '_blank');
 }
 
 $(document).scroll(function () {
-    if (($(this).scrollTop()+350) >= $('#donuts').offset().top) {
+    if (($(this).scrollTop() + 350) >= $('#donuts').offset().top) {
         $('#donuts').css('opacity', '1');
     }
-    if (($(this).scrollTop()+350) >= $('#manar').offset().top) {
+    if (($(this).scrollTop() + 350) >= $('#manar').offset().top) {
         $('#manar').css('opacity', '1');
     }
-    if (($(this).scrollTop()+350) >= $('#farm').offset().top) {
+    if (($(this).scrollTop() + 350) >= $('#farm').offset().top) {
         $('#farm').css('opacity', '1');
     }
 
-    $('.header_block').css({background: $(this).scrollTop() > $('.my_details').offset().top ? 'rgba(0, 0, 0,1)' : 'rgb(159, 159, 159)'});
+    // $('.header_block').css({background: $(this).scrollTop() > $('.my_details').offset().top ? 'rgba(0, 0, 0,1)' : 'rgb(159, 159, 159)'});
 });
